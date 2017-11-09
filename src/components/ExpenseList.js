73d606ae44aad6a462d2360769ef0a13fc9ae3c5
Qi Wu <Michaelwoo@QimatoMacBook-Air.local>
2017-11-08 21:31:26 -0800

    add expenses summary

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
-import selecteExpenses from '../selectors/expenses';
+import selectExpenses from '../selectors/expenses';
 
 export const ExpenseList = (props) => {
     return (
@@ -22,7 +22,7 @@ export const ExpenseList = (props) => {
 
 const mapStateToProps = (state) => {
     return {
-        expenses: selecteExpenses(state.expenses, state.filters)
+        expenses: selectExpenses(state.expenses, state.filters)
     };
 };
 
