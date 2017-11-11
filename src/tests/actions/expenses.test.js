c7b1ba0d29d38c6d07ef40174cbbefba507d63f5
Qi Wu <Michaelwoo@QimatoMacBook-Air.local>
2017-11-10 21:21:12 -0800

    setup startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -4,6 +4,7 @@ import {
     startAddExpense, 
     addExpense, 
     editExpense, 
+    startEditExpense,
     removeExpense, 
     startRemoveExpense,
     setExpenses, 
@@ -43,7 +44,7 @@ test('should remove expense from the database and store', (done) => {
         return database.ref(`expenses/${id}`).once('value');
     }).then((snapshot) => {
         expect(snapshot.val()).toBeFalsy();
-        done()
+        done();
     });
 });
 
@@ -60,6 +61,27 @@ test('should setup edit expense action object', () => {
         }
     });
 });
+
+test('should edit expense from firebase', (done) => {
+    const store = createMockStore();
+    const id = expenses[0].id;
+    const updates = {
+        amount: 21045
+    };
+    store.dispatch(startEditExpense(id, updates)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+
+        return database.ref(`expenses/${id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+    });
+})
 
 // Add Expense
 test('should setup add expense action object with provided values', () => {
